b2c8346152931a128dcf9aad5dddb8c57d4f935f
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-23 00:27:32 +0300

    FIX: bg file removed

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@
 import Header from "@/components/Header"
 import Hero from "@/components/Hero";
 import Image from "next/image";
-import heroBg from "../public/images/heroBg.png";
 import Industries from "@/components/Industries";
 import Footer from "@/components/Footer";
 
@@ -11,8 +10,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="flex flex-col gap-24 items-center justify-center m-auto">
-      <div className="w-full flex flex-col gap-16 bg-gray-100 items-center justify-center relative">
-        <Image src={heroBg} alt="grid" className="absolute -z-10" fill={true} />
+      <div className="w-full flex flex-col gap-16 bg-gray-100 items-center justify-center relative bg-hero-pattern">
         <Header />
         <Hero />
       </div>
