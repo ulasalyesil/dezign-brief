0ef4d1c5313700d57ff91e9b7851c73a23a402dc
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-22 22:51:25 +0300

    ADD: header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,12 @@ import GithubIcon from "@/public/icons/GithubIcon"
 
 export default function Header() {
   return (
-    <nav className="h-20 flex items-center justify-between">
+    <nav className="w-full  h-20 flex items-center justify-between px-6">
+        <div className="flex-1"/>
       <Link href={"/"} className="font-semibold text-gray-900">
         dezign.brief
       </Link>
+      <div className="flex-1"/>
       <Link href={'https://github.com/ulasalyesil/dezign-brief'}>
         <Button variant="outline" size="icon">
           <GithubIcon color={"#111827"} />
